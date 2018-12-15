aba31061d9180c4f0200b5a5074fc05c5753955a
Scott Halgrim <shalgrim@gmail.com>
2018-12-14 23:20:21 -0800

    day 14 part 2 finishes fast enough now

--- day14_2.py
+++ day14_2.py
@@ -13,18 +13,18 @@ logger.addHandler(StreamHandler(sys.stdout))
 
 def score_elves_backwards(string_to_find):
     scores = [3, 7]
-    string_scores = '37'
+    scores_to_find = [int(c) for c in string_to_find]
+    lookback = len(scores_to_find)
     elf_locations = [0, 1]
     last_power = 0
 
-    while string_to_find not in string_scores:
+    while scores_to_find != scores[-lookback:] and scores_to_find != scores[-lookback-1:-1]:
+        # print(scores)
         if floor(log10(len(scores))) > last_power:
             logger.info(f'len(scores): {len(scores)}')
             last_power = floor(log10(len(scores)))
         elf_scores = [scores[i] for i in elf_locations]
-        new_string_score = str(sum(elf_scores))
-        string_scores += new_string_score
-        scores += [int(c) for c in new_string_score]
+        scores += [int(c) for c in str(sum(elf_scores))]
 
         elf_moves = [1 + score for score in elf_scores]
         elf_locations = [
@@ -32,7 +32,13 @@ def score_elves_backwards(string_to_find):
             for location, move in zip(elf_locations, elf_moves)
         ]
 
-    score_index = string_scores.index(string_to_find)
+    # print(scores)
+    # print(scores_to_find)
+    if scores_to_find == scores[-lookback:]:
+        score_index = len(scores) - lookback
+    else:
+        score_index = len(scores) - lookback - 1
+    # print(f'score_index: {score_index}')
     return score_index
 
 
