4b6aad220f62e250936a46923fee0a6b0122a266
Scott Halgrim <shalgrim@gmail.com>
2018-12-15 21:33:44 -0800

    a little more work reading and writing caves

--- day15_1.py
+++ day15_1.py
@@ -1,10 +1,59 @@
+class Monster(object):
+    pass
+
+
+class Elf(Monster):
+    def __init__(self):
+        self.hp = 200
+
+    def __str__(self):
+        return f'E({self.hp})'
+
+
+class Goblin(Monster):
+    def __init__(self):
+        self.hp = 200
+
+    def __str__(self):
+        return f'G({self.hp})'
+
+
 class Cave(object):
     def __init__(self):
         self.elves = []
         self.goblins = []
+        self.walls = set()
+        self.floors = set()
 
     def tick(self):
         pass
+
+    def __str__(self):
+        max_y = max(w.y for w in self.walls)
+        max_x = max(w.x for w in self.walls)
+
+        lines = []
+        for y in range(max_y + 1):
+            line_chars = []
+            for x in range(max_x + 1):
+                if (x, y) in self.walls:
+                    line_chars.append('#')
+                elif (x, y) in self.elves:
+                    line_chars.append('E')
+                elif (x, y) in self.goblins:
+                    line_chars.append('G')
+                else:
+                    line_chars.append('.')
+
+            line = ''.join(line_chars)
+            monsters = [g for p, g in self.goblins.items() if p[1] == y]
+            monsters += [e for p, e in self.elves.items() if p[1] == y]
+            monsters.sort(key=lambda m: m.x)
+            if monsters:
+                line += '   '
+                line += ', '.join(str(m) for m in monsters)
+            lines.append(line)
+        return '\n'.join(lines) + '\n\n\n'
 
 
 def parse_cave_input(filename):
@@ -12,9 +61,24 @@ def parse_cave_input(filename):
         lines = f.readlines()
 
     cave = Cave()
-    for line in lines:
-        pass
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            if char == '#':
+                cave.walls.add((x, y))
+            elif char == '.':
+                cave.floors.add((x, y))
+            elif char == 'G':
+                cave.floors.add((x, y))
+                cave.goblins.append(Goblin())
+            elif char == 'E':
+                cave.floors.add((x, y))
+                cave.elves.append(Elf())
+
     return cave
+
+
+def calculate_cave_outcome(cave, completed_rounds):
+    pass
 
 
 def run_cave_game(cave):
@@ -22,9 +86,11 @@ def run_cave_game(cave):
     while any(elf.alive for elf in cave.elves) and any(
         goblin.alive for goblin in cave.goblins
     ):
+        print(cave)
         cave.tick()
         completed_rounds += 1
-    pass
+
+    return calculate_cave_outcome(cave, completed_rounds)
 
 
 if __name__ == '__main__':
