1adcf3c66db07e7c952a5a52ceca1a14c2ad3db7
Scott Halgrim <shalgrim@gmail.com>
2018-12-19 20:51:56 -0800

    taking a break to move on to 17 and 20

--- day19_2.py
+++ day19_2.py
@@ -15,16 +15,24 @@ logging.basicConfig(
 logger.addHandler(StreamHandler(sys.stdout))
 
 
-def run_program(instructions, instruction_pointer):
-    registers = [1] + [0] * 5
+def run_program(instructions, instruction_pointer, starting_registers=None):
+    if not starting_registers:
+        registers = [1] + [0] * 5
+    else:
+        registers = starting_registers
 
+    digit = False
     while 0 <= registers[instruction_pointer] < len(instructions):
         instruction = instructions[registers[instruction_pointer]]
         opcode = getattr(opcodes, instruction[0])
-        # output_line = f'ip={registers[instruction_pointer]} {registers} {" ".join([str(i) for i in instruction])} '
+        if registers[instruction_pointer] == 13:
+            output_line = f'ip={registers[instruction_pointer]} {registers} {" ".join([str(i) for i in instruction])} '
+            digit = True
         registers = opcode(registers, *instruction[1:])
-        # output_line += str(registers)
-        # logger.info(output_line)
+        if digit:
+            output_line += str(registers)
+            logger.info(output_line)
+            digit = False
         registers[instruction_pointer] += 1
 
     return registers
@@ -32,6 +40,8 @@ def run_program(instructions, instruction_pointer):
 
 def main(filename):
     instruction_pointer, instructions = parse_input_day19(filename)
+    starting_registers = [6, 10551289, 13, 10551288, 4, 1]
+    # registers = run_program(instructions, instruction_pointer, starting_registers)
     registers = run_program(instructions, instruction_pointer)
     return registers[0]
 
@@ -39,3 +49,5 @@ def main(filename):
 if __name__ == '__main__':
     print(main('data/input19.txt'))
     # 2280 is too low
+    # 55664833953828 is too high
+    # 55664844505116 is also too high obviously but i guessed it anyway
