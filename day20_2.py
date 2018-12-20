cb7dc6e53c097fcb9cf668ca993bd838b42c7be0
Scott Halgrim <shalgrim@gmail.com>
2018-12-20 00:00:50 -0800

    figured out part two

--- day20_2.py
+++ day20_2.py
@@ -1,13 +1,15 @@
 from day20_1 import ALL_STATES, find_furthest_room_helper
 
-
 def find_num_rooms_beyond_distance(regex, distance):
     ALL_STATES.clear()
     output = find_furthest_room_helper(regex)
     end_states_beyond_distance = [
         es for es in output.values() if es.distance >= distance
     ]
-    return len(set(end_states_beyond_distance))
+    all_states_beyond_distance = [
+        state for state in ALL_STATES.values() if state.distance >= distance
+    ]
+    return len(set(end_states_beyond_distance)), len(all_states_beyond_distance)
 
 
 if __name__ == '__main__':
@@ -15,3 +17,4 @@ if __name__ == '__main__':
         data = f.read().strip()
     print(f'answer: {find_num_rooms_beyond_distance(data, 1000)}')
     # 630 is too small
+    # as is my guess of 1500
