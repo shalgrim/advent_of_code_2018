8315be6e97ee4353a6166a98b00fca48cd55100b
Scott Halgrim <shalgrim@gmail.com>
2019-12-08 10:39:32 -0800

    day 19: trying to reboot, get logging to work

--- day19_2.py
+++ day19_2.py
@@ -16,6 +16,7 @@ logger.addHandler(StreamHandler(sys.stdout))
 
 
 def run_program(instructions, instruction_pointer, starting_registers=None):
+    """Use starting_registers to start mid-execution at known point"""
     if not starting_registers:
         registers = [1] + [0] * 5
     else:
@@ -25,14 +26,24 @@ def run_program(instructions, instruction_pointer, starting_registers=None):
     while 0 <= registers[instruction_pointer] < len(instructions):
         instruction = instructions[registers[instruction_pointer]]
         opcode = getattr(opcodes, instruction[0])
-        if registers[instruction_pointer] == 13:
-            output_line = f'ip={registers[instruction_pointer]} {registers} {" ".join([str(i) for i in instruction])} '
-            digit = True
+        output_line = f'ip={registers[instruction_pointer]} {registers} {" ".join([str(i) for i in instruction])} '
+
+        # this exists to log known interesting configurations
+        # if registers[instruction_pointer] == 13:
+        #     output_line = f'ip={registers[instruction_pointer]} {registers} {" ".join([str(i) for i in instruction])} '
+        #     digit = True
+
         registers = opcode(registers, *instruction[1:])
-        if digit:
-            output_line += str(registers)
-            logger.info(output_line)
-            digit = False
+
+        output_line += str(registers)
+        logger.info(output_line)
+
+        # this exists to log known interesting configurations
+        # if digit:
+        #     output_line += str(registers)
+        #     logger.info(output_line)
+        #     digit = False
+
         registers[instruction_pointer] += 1
 
     return registers
@@ -40,11 +51,16 @@ def run_program(instructions, instruction_pointer, starting_registers=None):
 
 def main(filename):
     instruction_pointer, instructions = parse_input_day19(filename)
+    starting_registers = None
     # starting_registers = [6, 10551289, 13, 10551288, 4, 1]
-    starting_registers = [864, 10551289, 13, 10551288, 157, 1]
-    # starting_registers = [0] * 6  # and that works
+    # starting_registers = [864, 10551289, 13, 10551288, 157, 1]
+    # starting_registers = [1051, 10551289, 13, 10551288, 192, 1]
+    # starting_registers = [1255, 10551289, 13, 10551288, 264, 1]
+    # starting_registers = [1519, 10551289, 13, 10551288, 264, 1]
+    # starting_registers = [3610, 10551289, 13, 10551288, 921, 1]
+    # starting_registers = [3610, 10551289, 13, 10551288, 1018, 1]
+    # starting_registers = [0] * 6  # use this to test that this code still gives you 2280 for part 1
     registers = run_program(instructions, instruction_pointer, starting_registers)
-    # registers = run_program(instructions, instruction_pointer)
     return registers[0]
 
 
