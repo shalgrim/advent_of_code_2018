1e24b75fc95c4b95229b1b52a1e633f6d7c2dcd1
Scott Halgrim <shalgrim@gmail.com>
2020-05-02 20:14:22 -0700

    some refactoring

--- day21_alternate.py
+++ day21_alternate.py
@@ -1,32 +1,39 @@
 REG0 = 4_682_012
-ONE_THEN_16_ZEROS = 65_536  # 2^16
+ONE_THEN_16_ZEROS = 65536  # 2^16
 EIGHT_ONES = 255  # 2^8 - 1
 TWENTY_FOUR_ONES = 16_777_215  # 2^24 - 1
 BIG_RANDO = 8_725_355
-SMALL_RANDO = 65_899
+SMALL_RANDO = 65899
+
+
+def mod(x, y):
+    return x & y - 1
+
+
+def big_fat_one(r1, r2):
+    return mod(
+        mod(mod(r2, EIGHT_ONES + 1) + r1, TWENTY_FOUR_ONES + 1) * SMALL_RANDO,
+        TWENTY_FOUR_ONES + 1,
+    )
 
 
 def main():
-    reg0 = REG0  # This never changes
+    reg0 = REG0  # This never changes after it's set
     reg1 = 0
     reg2 = 0
     reg3 = 0
-    reg4 = -1  # Don't really need this
+    reg4 = -1  # IP...don't really need this
     reg5 = 0
 
     while reg0 != reg1:  # I06 loop
         # I06 - addition except when the 2^16 bit is turned on in reg1 then it's the original number
-        reg2 = reg1 | ONE_THEN_16_ZEROS  # I06 - addition except when the 2^16 bit is turned on in reg1 then it's
+        reg2 = (
+            reg1 | ONE_THEN_16_ZEROS
+        )  # I06 - addition except when the 2^16 bit is turned on in reg1 then it's
         reg1 = BIG_RANDO  # I07
 
         # do part of do-while loop
-        reg5 = reg2 & EIGHT_ONES  # I08
-        reg1 += reg5  # I09
-
-        # Can these lines be concisified?
-        reg1 = reg1 & TWENTY_FOUR_ONES  # I10
-        reg1 *= SMALL_RANDO  # I11
-        reg1 = reg1 & TWENTY_FOUR_ONES  # I12
+        reg1 = big_fat_one(reg1, reg2)
 
         while EIGHT_ONES < reg2:  # I08 loop
             # invariant 256 <= reg2
@@ -39,14 +46,19 @@ def main():
             while reg3 <= reg2:
                 reg5 += 1
                 reg3 = reg5 + 1
-                reg3 *= 256  # so this is like reg3 going up by 512 each time until it's bigger than reg2?
+                reg3 *= (
+                    256
+                )  # so this is like reg3 going up by 512 each time until it's bigger than reg2?
+                # So could I get at this with something like
+                # reg5 = reg2 / 512  (that's not quite right)
 
-            reg2 = reg5  # I26; and then set the number of times it had to increase to reg2
+            reg2 = (
+                reg5
+            )  # I26; and then set the number of times it had to increase to reg2
 
-            # Back to that Do part but conciser
-            # So something about the number of times we did that combined with where reg1 was
+            # something about the number of times we did that (big_fat_one?) combined with where reg1 was
             # and putting that back in reg1
-            reg1 = ((((reg2 & EIGHT_ONES) + reg1) & TWENTY_FOUR_ONES) * SMALL_RANDO) & TWENTY_FOUR_ONES
+            reg1 = big_fat_one(reg1, reg2)
 
     return reg0, reg1, reg2, reg3, reg4, reg5
 
