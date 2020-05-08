7319a80ee75e0f4f9c87ead5f5e1bc83636635b1
Scott Halgrim <shalgrim@gmail.com>
2020-05-07 19:34:53 -0700

    bug fix: track visited
    still erroring tho

--- day22_2.py
+++ day22_2.py
@@ -1,3 +1,4 @@
+from copy import copy
 from enum import Enum, auto
 
 from day22_1 import CAVE_DEPTH, TARGET_X, TARGET_Y, RegionType, build_cave
@@ -53,11 +54,7 @@ class PathFinder:
         else:
             raise Exception('should not be here')
 
-    def move(self, direction):
-        next_position = self._get_next_position(direction)
-
-        if not next_position:
-            return False
+    def move(self, next_position):
         current_region_type = self.cave[self.current_position].tipe
         next_region_type = self.cave[next_position].tipe
         possible_equipment = self.get_possible_equipment(
@@ -73,7 +70,9 @@ class PathFinder:
 
         return move_cost
 
-    def find_quickest_path(self, cost=0):
+    def find_quickest_path(self, cost=0, visited=None):
+        myvisited = copy(visited) if visited is not None else []
+        myvisited.append(self.current_position)
         if self.current_position == self.target_position:
             final_cost = cost if self.current_equipment == Equipment.TORCH else cost + 7
             if final_cost < self.known_shortest_path:
@@ -84,17 +83,20 @@ class PathFinder:
             return
 
         for d in Direction:
-            move_cost = self.move(d)
-            if move_cost:
-                self.find_quickest_path(cost + move_cost)
+            next_position = self._get_next_position(d)
+            if not next_position or next_position in myvisited:
+                return False
+
+            move_cost = self.move(next_position)
+            self.find_quickest_path(cost + move_cost, myvisited)
 
     def _set_baseline_shortest_path(self):
         """go straight there, down then right"""
         cost = 0
         while self.current_position[0] < self.target_position[0]:
-            cost += self.move(Direction.RIGHT)
+            cost += self.move(self._get_next_position(Direction.RIGHT))
         while self.current_position[1] < self.target_position[1]:
-            cost += self.move(Direction.DOWN)
+            cost += self.move(self._get_next_position(Direction.DOWN))
 
         self.current_position = 0, 0
 
