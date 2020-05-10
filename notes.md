49d48fa522ef88d8725ecd35bbf4cc63eb554cbe
Scott Halgrim <shalgrim@gmail.com>
2020-05-09 20:10:43 -0700

    working backward from base, finding failures

--- notes.md
+++ notes.md
@@ -28,3 +28,12 @@ Problem:
     - oof
     - and the reason I wanted to do that was to record a global known_shortest_path
     - and that's nice, but it doesn't jibe with the distances/actual_distances/shortest_froms work I'm doing in lines 245-253
+
+- Working through it from `test_base_case_minus_7`
+- The happy path will be taking Down as the first direction
+- But that will be the third direction it tries, after Right and Up
+- It gets there okay and drills down into a state of (6, 12), CLIMB, and the previous test case found the shortest path from there fine
+- So what goes wrong
+- I feel like it has to be a `shortest_from` gets set wrong
+- Yep, `shortest_froms['(8, 12),Equipment.CLIMB']` is set to 137
+- So next in to test this test case and figure out why that gets set the way it does even though `test_case_base_minus_4` succeeds
--- test_day22.py
+++ test_day22.py
@@ -1,9 +1,7 @@
-import sys
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from day22_1 import CAVE_DEPTH, TARGET_X, TARGET_Y, build_cave, calc_risk_level
-from day22_2 import PathFinder
-
+from day22_2 import Equipment, PathFinder, State
 
 TEST_TARGET_X = 10
 TEST_TARGET_Y = 10
@@ -15,10 +13,68 @@ class TestDay22(TestCase):
         self.actual_cave = build_cave(TARGET_X, TARGET_Y, CAVE_DEPTH)
         self.test_cave = build_cave(TEST_TARGET_X, TEST_TARGET_Y, TEST_CAVE_DEPTH)
 
+    @skip
     def test_part_one(self):
-        self.assertEqual(calc_risk_level(TEST_TARGET_X, TEST_TARGET_Y, self.test_cave), 114)
+        self.assertEqual(
+            calc_risk_level(TEST_TARGET_X, TEST_TARGET_Y, self.test_cave), 114
+        )
         self.assertEqual(calc_risk_level(TARGET_X, TARGET_Y, self.actual_cave), 9659)
 
+    def test_part_two_base_case(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(10, 10), equipment=Equipment.TORCH, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 0)
+
+    def test_part_two_base_case_plus_switch(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(10, 10), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 7)
+
+    def test_base_case_minus_1(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(10, 11), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 8)
+
+    def test_base_case_minus_2(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(10, 12), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 9)
+
+    def test_base_case_minus_3(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(9, 12), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 10)
+
+    def test_base_case_minus_4(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(8, 12), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 11)
+
+    def test_base_case_minus_6(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(6, 12), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 13)
+
+    def test_base_case_minus_7(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(6, 11), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 14)
+
+    def test_base_case_minus_8(self):
+        pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
+        state = State(position=(5, 11), equipment=Equipment.CLIMB, visited=[], cost=0)
+        pf.find_quickest_path(state)
+        self.assertEqual(pf.known_shortest_path, 15)
+
+    @skip("not ready")
     def test_part_two(self):
         pf = PathFinder(self.test_cave, TEST_TARGET_X, TEST_TARGET_Y)
         pf.find_quickest_path()
