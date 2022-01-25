ccfd511d016acc08e9eeb70bc607a3afee714535
Scott Halgrim <shalgrim@gmail.com>
2022-01-24 16:01:28 -0800

    day 24 success

--- test_day24.py
+++ test_day24.py
@@ -6,3 +6,7 @@ from day24_1 import day24_1
 class TestDay24(TestCase):
     def test_day24_1(self):
         self.assertEqual(day24_1('data/test24.txt'), 5216)
+        self.assertEqual(day24_1('data/input24.txt'), 10538)
+
+    def test_day24_2(self):
+        self.assertEqual(day24_1('data/test24.txt', 1570), 51)
